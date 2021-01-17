bb29f67d8c654c25de7a80a5feba833cb70081f6
Dikamjit Borah <learningdjb99@gmail.com>
2021-01-18 00:52:04 +0530

    Updates

---
+++ precache-manifest.81d7d36f1bf10c74ee7e61569f1ed826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a281c908065e961a42d9cd30b9fea4b8",
+    "url": "/TrailSec/index.html"
+  },
+  {
+    "revision": "706eea0262197d9e16d8",
+    "url": "/TrailSec/static/css/main.8da79239.chunk.css"
+  },
+  {
+    "revision": "bf2f04cb5c933c70d824",
+    "url": "/TrailSec/static/js/2.708a2c72.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/TrailSec/static/js/2.708a2c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706eea0262197d9e16d8",
+    "url": "/TrailSec/static/js/main.f68d93f3.chunk.js"
+  },
+  {
+    "revision": "6b06c8ed9f9f82ddce84",
+    "url": "/TrailSec/static/js/runtime-main.213bc08a.js"
+  }
+]);
